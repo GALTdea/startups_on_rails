36b624b30bfa4b4193aeec50499f62012ef97e3c
Gustavo Valenzuela <gustavovalenzuela1180@gmail.com>
2025-02-13 13:40:34 -0800

    feat: add role enum to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,14 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  enum role: {
+    member: 0,        # Regular community member
+    company_owner: 1, # Tech company representative
+    admin: 2          # Platform administrator
+  }
+
+  after_initialize :set_default_role, if: :new_record?
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 end
