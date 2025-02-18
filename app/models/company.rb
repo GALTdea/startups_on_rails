b55eab485054c2b877c81be69de80f36bbe3cdd7
Gustavo Valenzuela <gustavovalenzuela1180@gmail.com>
2025-02-18 10:38:14 -0800

    Add validations and associations to Company model

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,3 +1,9 @@
 class Company < ApplicationRecord
-  belongs_to :user
+  belongs_to :owner, class_name: "User", foreign_key: "user_id"
+
+
+  validates :name, :description, :website, presence: true
+  validates :slug, uniqueness: true
+
+  scope :published, -> { where(published: true) }
 end
