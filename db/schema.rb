33fac551d0729b51a734763ac3bb7e5e979498af
Gustavo Valenzuela <gustavovalenzuela1180@gmail.com>
2025-02-18 11:05:56 -0800

    chore: update schema with companies table

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_02_13_213635) do
+ActiveRecord::Schema[8.0].define(version: 2025_02_18_181607) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
+
+  create_table "companies", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.string "name"
+    t.text "description"
+    t.string "website"
+    t.string "location"
+    t.string "slug"
+    t.boolean "published"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_companies_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -26,4 +39,6 @@ ActiveRecord::Schema[8.0].define(version: 2025_02_13_213635) do
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
+
+  add_foreign_key "companies", "users"
 end
