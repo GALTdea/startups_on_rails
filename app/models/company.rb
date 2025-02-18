0816c49987302870bf98312ea8ceb2508535d71c
Gustavo Valenzuela <gustavovalenzuela1180@gmail.com>
2025-02-18 14:27:31 -0800

    Update Company model validation and admin check logic

--- app/models/company.rb
+++ app/models/company.rb
@@ -2,20 +2,24 @@ class Company < ApplicationRecord
   belongs_to :owner, class_name: "User", foreign_key: "user_id", optional: true
 
   validates :name, :description, :website, presence: true
-  validates :slug, uniqueness: true
+  # validates :slug, uniqueness: true
   validate :require_owner_unless_admin
+  # validates :description, presence: true, length: { minimum: 20 }
 
   scope :published, -> { where(published: true) }
+
+  attr_accessor :created_by
 
   private
 
   def require_owner_unless_admin
+    Rails.logger.info "Validation check - Admin?: #{created_by_admin?}, User ID: #{user_id}"
     return if user_id.present? || created_by_admin?
 
     errors.add(:user_id, "must be present for non-admin created companies")
   end
 
-  def created_by_admin
-    @created_by_admin ||= User.current.admin?
+  def created_by_admin?
+    @created_by_admin ||= created_by&.admin?
   end
 end
