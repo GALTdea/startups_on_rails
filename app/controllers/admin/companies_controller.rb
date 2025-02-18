319ead3cd3767df3abefe484bd8735b7b92f2238
Gustavo Valenzuela <gustavovalenzuela1180@gmail.com>
2025-02-18 14:28:25 -0800

    Update CompaniesController to handle Turbo Stream responses

--- app/controllers/admin/companies_controller.rb
+++ app/controllers/admin/companies_controller.rb
@@ -18,13 +18,18 @@ class Admin::CompaniesController < ApplicationController
 
   def create
     @company = Company.new(company_params)
-    @company.owner = current_user unless current_user.admin? # Only set owner if not admin
+    @company.created_by = current_user
+    @company.owner = current_user unless current_user.admin?
 
-    if @company.save
-      redirect_to admin_company_path(@company), notice: "Company was successfully created."
-    else
-      @users = User.company_owners if current_user.admin?
-      render :new
+    respond_to do |format|
+      if @company.save
+        format.html { redirect_to admin_company_path(@company), notice: "Company was successfully created." }
+        format.turbo_stream { redirect_to admin_company_path(@company) }
+      else
+        @users = User.company_owners if current_user.admin?
+        format.html { render :new }
+        format.turbo_stream { render turbo_stream: turbo_stream.replace("company_form", partial: "form", locals: { company: @company }) }
+      end
     end
   end
 
@@ -48,5 +53,9 @@ class Admin::CompaniesController < ApplicationController
     return if current_user.admin? || current_user.company_owner?
 
     redirect_to root_path, alert: "Not authorized!"
+  end
+
+  def set_company
+    @company = Company.find(params[:id])
   end
 end
