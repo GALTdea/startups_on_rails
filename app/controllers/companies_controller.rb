eb0098f1e765eef661f477ae8c979217ba1fdba3
Gustavo Valenzuela <gustavovalenzuela1180@gmail.com>
2025-02-21 09:34:13 -0800

    completes companies controller actions

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -1,20 +1,42 @@
 class CompaniesController < ApplicationController
+  before_action :set_company, only: [ :show ]
+
   def index
-    @companies = Company.published.order(created_at: :desc)
+    @companies = Company.published
+                         .includes(:tags, :categories)
+                         .order(created_at: :desc)
 
-    if params[:search].present?
-      @companies = @companies.search(params[:search])
-    end
-
-    if params[:category].present?
-      @companies = @companies.joins(:categories).where(categories: { id: params[:category] })
-    end
-
-    if params[:tag].present?
-      @companies = @companies.joins(:tags).where(tags: { id: params[:tag] })
-    end
+    apply_search_filter
+    apply_category_filter
+    apply_tag_filter
   end
 
   def show
+    authorize @company
+  end
+
+  private
+
+  def set_company
+    @company = Company.published.find(params[:id])
+  rescue ActiveRecord::RecordNotFound
+    redirect_to companies_path, alert: "Company not found"
+  end
+
+  def apply_search_filter
+    return unless params[:search].present?
+    @companies = @companies.search(params[:search])
+  end
+
+  def apply_category_filter
+    return unless params[:category].present?
+    @companies = @companies.joins(:categories)
+                          .where(categories: { id: params[:category] })
+  end
+
+  def apply_tag_filter
+    return unless params[:tag].present?
+    @companies = @companies.joins(:tags)
+                          .where(tags: { id: params[:tag] })
   end
 end
