f06e472ab51f9462e0804217c8a1ba9d38e38d20
Gustavo Valenzuela <gustavovalenzuela1180@gmail.com>
2025-02-21 09:08:33 -0800

    Adds companies resources and root page

--- config/routes.rb
+++ config/routes.rb
@@ -23,5 +23,6 @@ Rails.application.routes.draw do
   # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
 
   # Defines the root path route ("/")
-  root "pages#index"
+  resources :companies, only: [ :index, :show ]
+  root to: "companies#index"
 end
