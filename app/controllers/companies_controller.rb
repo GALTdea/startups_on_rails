c3700f39c74a7fa1b2a0213420824e0fb925015f
Gustavo Valenzuela <gustavovalenzuela1180@gmail.com>
2025-02-21 09:06:33 -0800

    Adds companies index action

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -1,5 +1,18 @@
 class CompaniesController < ApplicationController
   def index
+    @companies = Company.published.order(created_at: :desc)
+
+    if params[:search].present?
+      @companies = @companies.search(params[:search])
+    end
+
+    if params[:category].present?
+      @companies = @companies.joins(:categories).where(categories: { id: params[:category] })
+    end
+
+    if params[:tag].present?
+      @companies = @companies.joins(:tags).where(tags: { id: params[:tag] })
+    end
   end
 
   def show
