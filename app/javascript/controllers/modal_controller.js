1f392ea0d9ab22883e1b586dd11098fadab3e2b2
Gustavo Valenzuela <gustavovalenzuela1180@gmail.com>
2025-02-24 12:28:09 -0800

    Refactor modal controller to handle form submissions and improve dropdown closing

--- app/javascript/controllers/modal_controller.js
+++ app/javascript/controllers/modal_controller.js
@@ -6,10 +6,14 @@ export default class extends Controller {
     connect() {
         // Close dropdown when clicking outside
         document.addEventListener('click', this.handleClickOutside.bind(this))
+
+        // Listen for form submissions
+        document.addEventListener('turbo:submit-start', this.handleFormSubmit.bind(this))
     }
 
     disconnect() {
         document.removeEventListener('click', this.handleClickOutside.bind(this))
+        document.removeEventListener('turbo:submit-start', this.handleFormSubmit.bind(this))
     }
 
     toggle(event) {
@@ -26,19 +30,30 @@ export default class extends Controller {
             this.dropdownTarget.classList.remove('opacity-0', 'scale-95')
             this.dropdownTarget.classList.add('opacity-100', 'scale-100')
         } else {
-            this.dropdownTarget.classList.remove('opacity-100', 'scale-100')
-            this.dropdownTarget.classList.add('opacity-0', 'scale-95')
-
-            setTimeout(() => {
-                this.dropdownTarget.classList.add('hidden')
-            }, 100)
+            this.closeDropdown()
         }
+    }
+
+    closeDropdown() {
+        this.dropdownTarget.classList.remove('opacity-100', 'scale-100')
+        this.dropdownTarget.classList.add('opacity-0', 'scale-95')
+
+        setTimeout(() => {
+            this.dropdownTarget.classList.add('hidden')
+        }, 100)
     }
 
     handleClickOutside(event) {
         if (!this.element.contains(event.target)) {
-            this.dropdownTarget.classList.add('hidden')
-            this.dropdownTarget.classList.remove('opacity-100', 'scale-100')
+            this.closeDropdown()
         }
+    }
+
+    handleFormSubmit(event) {
+        // Don't close if it's not our form
+        if (!this.element.contains(event.target)) return
+
+        // Close dropdown after form submission
+        this.closeDropdown()
     }
 } 
