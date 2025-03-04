0d9c21e11c3edfbfd3e39689c3f75eabd416df23
Gustavo Valenzuela <gustavovalenzuela1180@gmail.com>
2025-03-04 15:24:56 -0800

    Add solution views and form partial: update index, show, edit, new views and extract form to partial for DRY code

--- app/helpers/solutions_helper.rb
+++ app/helpers/solutions_helper.rb
@@ -1,2 +1,18 @@
 module SolutionsHelper
+  def solution_type_badge_class(solution_type)
+    case solution_type&.downcase
+    when "product"
+      "bg-blue-100 text-blue-800"
+    when "service"
+      "bg-green-100 text-green-800"
+    when "platform"
+      "bg-purple-100 text-purple-800"
+    when "tool"
+      "bg-yellow-100 text-yellow-800"
+    when "framework"
+      "bg-indigo-100 text-indigo-800"
+    else
+      "bg-gray-100 text-gray-800"
+    end
+  end
 end
