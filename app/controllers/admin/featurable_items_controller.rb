7c2e23fc7d1dca6c86b9f469a4b3118086984e64
Gustavo Valenzuela <gustavovalenzuela1180@gmail.com>
2025-03-18 13:02:15 -0700

    Add search and filtering to featurable items selection with category matching and Turbo Stream support

--- app/controllers/admin/featurable_items_controller.rb
+++ app/controllers/admin/featurable_items_controller.rb
@@ -3,38 +3,54 @@ class Admin::FeaturableItemsController < Admin::BaseController
   before_action :set_featurable_item, only: [ :create, :destroy ]
 
   def index
+    @featured_listing = FeaturedListing.find(params[:featured_listing_id])
     @type = params[:type] || "Company"
-    @query = params[:query]
+    @search = params[:search]
 
-    base_query = case @type
-    when "Company"
-                   Company.published
-                         .joins(:categorizables)
-                         .where(categorizables: { category_id: @featured_listing.category_id })
-                         .where.not(id: FeaturedListing.where(featurable_type: "Company").select(:featurable_id))
-    when "Solution"
-                   Solution.published
-                          .joins(:categorizables)
-                          .where(categorizables: { category_id: @featured_listing.category_id })
-                          .where.not(id: FeaturedListing.where(featurable_type: "Solution").select(:featurable_id))
-    else
-                   Company.published
-                         .joins(:categorizables)
-                         .where(categorizables: { category_id: @featured_listing.category_id })
-                         .where.not(id: FeaturedListing.where(featurable_type: "Company").select(:featurable_id))
-    end
+    # Start with the base class
+    klass = @type.constantize
 
-    @featurable_items = if @query.present?
-      base_query.class.search(@query)
-    else
-      base_query
-    end
-
-    # Debug information
-    Rails.logger.debug "Featured Listing Category ID: #{@featured_listing.category_id}"
+    # Log initial parameters
+    Rails.logger.debug "Search Parameters:"
     Rails.logger.debug "Type: #{@type}"
-    Rails.logger.debug "Query: #{@query}"
-    Rails.logger.debug "Items Count: #{@featurable_items.count}"
+    Rails.logger.debug "Search Term: #{@search}"
+    Rails.logger.debug "Category ID: #{@featured_listing.category_id}"
+
+    # Build query in steps for debugging
+    @items = klass.published
+    Rails.logger.debug "After published scope: #{@items.to_sql}"
+
+    # Exclude already featured items
+    already_featured = FeaturedListing.where(featurable_type: @type).pluck(:featurable_id)
+    @items = @items.where.not(id: already_featured) if already_featured.any?
+    Rails.logger.debug "After excluding featured: #{@items.to_sql}"
+
+    # Add search condition if present
+    if @search.present?
+      @items = @items.where(
+        "LOWER(#{klass.table_name}.name) ILIKE :search OR LOWER(#{klass.table_name}.description) ILIKE :search",
+        search: "%#{@search.downcase}%"
+      )
+      Rails.logger.debug "After search condition: #{@items.to_sql}"
+    end
+
+    @items = @items.distinct
+
+    # For each item, check if it belongs to the correct category
+    @items = @items.map do |item|
+      item_categories = item.categories.pluck(:id) + item.categorizables.pluck(:category_id)
+      item.instance_variable_set(:@category_matches, item_categories.include?(@featured_listing.category_id))
+      item.define_singleton_method(:category_matches?) { @category_matches }
+      item
+    end
+
+    # Log final count
+    Rails.logger.debug "Final item count: #{@items.count}"
+
+    respond_to do |format|
+      format.html
+      format.turbo_stream
+    end
   end
 
   def create
