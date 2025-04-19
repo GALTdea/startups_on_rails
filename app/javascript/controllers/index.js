edb92d52b0a4ec1b5f6788d2e8c67a4701b3acfd
Gustavo Valenzuela <gustavovalenzuela1180@gmail.com>
2025-04-19 12:33:01 -0700

    fix-stimulus- update JavaScript configuration: Modify importmap and controllers setup for enhanced frontend functionality

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -1,10 +1,11 @@
-import { application } from "./application"
+import { application } from "controllers/application"
+
 import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading"
-
-import BottomSheetController from "./bottom_sheet_controller"
-import StickySearchController from "./sticky_search_controller"
-
 eagerLoadControllersFrom("controllers", application)
 
-application.register("bottom-sheet", BottomSheetController)
-application.register("sticky-search", StickySearchController)
+
+// import BottomSheetController from "./bottom_sheet_controller"
+// import StickySearchController from "./sticky_search_controller"
+
+// application.register("bottom-sheet", BottomSheetController)
+// application.register("sticky-search", StickySearchController)
